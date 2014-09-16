e0a87af36dd519a41e4f8c33c4d6795029590983
geoyws <geoyws@gmail.com>
2014-09-17 02:24:49 +0800

    finally

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.get('/', function(req, res) {
 io.on('connection', function(socket) {
   console.log('A user connected.');
   socket.on('chatmsg', function(msg) {
+    io.emit('chatmsg', msg);
     console.log('Message: ' + msg);
   });
   socket.on('disconnect', function() {
